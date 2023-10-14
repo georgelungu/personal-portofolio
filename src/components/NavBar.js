c826483090ec49654176dd277c0eab29912ecfb0
George Lungu <george.lungu1896@gmail.com>
2023-10-14 15:31:42 +0300

    starter code on navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+export const NavBar = () => 
+{
+    const [activeLink, setActiveLink] = useState('home')
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) 
+                setScrolled(true)
+            else 
+                setScrolled(false)
+        }
+
+        window.addEventListener("scroll", onScroll)
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [])
+
+    return(
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={''} alt="Logo"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className='navbar-toggler-icon'></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                    </Nav>
+                    <span className='navbar-text'>
+                        <div className='social-icon'>
+                            <a href='#'>
+                                <img src={''} alt=''/>
+                            </a>
+                            <a href='#'>
+                                <img src={''} alt=''/>
+                            </a>
+                            <a href='#'>
+                                <img src={''} alt=''/>
+                            </a>
+                        </div>
+                        <buton className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></buton>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
