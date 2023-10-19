650fa6d4e20ab86d398e638c7b1e692c0d8708a3
George Lungu <george.lungu1896@gmail.com>
2023-10-19 11:44:12 +0300

    finished the banner and the skills section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,63 @@
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImage from '../assets/img/header-img.svg'
 
 export const Banner = () => {
+
+    const [loopNum, setLoopNum] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const toRotate = ["MERN Developer", "Web Developer", "Junior Front-End Developer"]
+    const [text, setText] = useState('')
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    const period = 2000
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick()
+        }, delta)
+        return () => {
+            clearInterval(ticker)
+        }
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+        setText(updatedText)
+
+        if (isDeleting)
+        {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText)
+        {
+            setIsDeleting(true)
+            setDelta(period)
+        }
+        else if (isDeleting && updatedText === '')
+        {
+            setIsDeleting(false)
+            setLoopNum(loopNum + 1)
+            setDelta(300);
+        }
+    }
+
     return ( 
         <section className="banner" id="home">
             <Container>
-                <Row>
-                    <Col>
-                    
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portofolio</span>
+                        <h1>{`Hi I'm George `}<span className="wrap">{text}</span></h1>
+                        <p>Here I will insert a little paragraph with maximum 3 short sentences about me.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImage} alt="Header Image"/>
                     </Col>
                 </Row>
             </Container>
